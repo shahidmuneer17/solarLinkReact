5abb699a22bbea772b1099c8c4cf929370bf3696
Metaxuae <media@metaxuae.com>
2025-06-03 00:26:12 +0400

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './',  // <-- Important for Netlify relative paths
   build: {
-    outDir: 'dist'
-  }
+    outDir: 'dist',
+  },
 });
